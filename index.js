1b26911e03ca96ad61132d87ea866912c37f58a9
ericksebas24 <60784826+ericksebas24@users.noreply.github.com>
2021-06-28 11:53:09 -0500

    avance

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-cc
+const { Router } = require('express');
+const express = require('express');
+//genero una aplicacion express
+
+const app = express()
+// routes
+const { RouterIndex } = require('./ROUTES/index');
+
+app.use("/", RouterIndex)
+app.use("/prueba", RouterIndex)
+
+/*
+        "/"+"/"="//" => "/"
+        "/"+"saludo" = "//Saludo" => "/saludo"
+        "/"+ "'/saludo/:nombre'" = //saludo/:nombre => "/saludo/:nombre"
+        "/user" + "/saludo" = "/user/saludo"
+        "user/data" + "/saludo" = "/user/data/saludo"
+*/
+app.listen(3000, ()=>{
+    console.log("Servidor escuchando en htpp://localhost:3000");
+})
